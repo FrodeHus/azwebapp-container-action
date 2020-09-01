67da4c2bbf5b1382a6fd1adfc54f8b3bfb3ef270
frodehus <frode.hus@outlook.com>
2020-09-01 21:22:50 +0200

    added missing registry name

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ echo "Logging into Azure..."
 az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p ${INPUT_SERVICE_PRINCIPAL_PASSWORD} --tenant ${INPUT_TENANT}
 
 echo "Deploying ${INPUT_IMAGE}:${INPUT_TAG} to ${INPUT_WEBAPP_NAME}@${INPUT_RESOURCE_GROUP}"
-az webapp config container set --docker-custom-image-name ${INPUT_IMAGE}:${INPUT_TAG} --docker-registry-server-url https://${INPUT_REGISTRY}.azurecr.io --name ${INPUT_WEBAPP_NAME} --resource-group ${INPUT_RESOURCE_GROUP}
+az webapp config container set --docker-custom-image-name ${INPUT_REGISTRY}.azurecr.io/${INPUT_IMAGE}:${INPUT_TAG} --docker-registry-server-url https://${INPUT_REGISTRY}.azurecr.io --name ${INPUT_WEBAPP_NAME} --resource-group ${INPUT_RESOURCE_GROUP}
